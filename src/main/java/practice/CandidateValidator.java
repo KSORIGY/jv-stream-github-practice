fbf5a2f59fffeade7a1470b736beeacd52da13c7
Denny Hirniak <denforever777@gmail.com>
2026-01-15 18:01:54 +0200

    solution stream-hw

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,37 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int MIN_TIME_LIVING_IN_UA = 35;
+    /**
+     * Your help with a election is needed. Given list of candidates, where each element
+     * has Candidate.class type.
+     * Check which candidates are eligible to apply for president position and return their
+     * names sorted alphabetically.
+     * The requirements are: person should be older than 35 years, should be allowed to vote,
+     * have nationality - 'Ukrainian'
+     * and live in Ukraine for 10 years. For the last requirement use field periodsInUkr,
+     * which has following view: "2002-2015"
+     * We want to reuse our validation in future, so let's write our own impl of Predicate
+     * parametrized with Candidate in CandidateValidator.
+     */
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] livingYears = candidate.getPeriodsInUkr().trim().split("-");
+        int startLiving = Integer.parseInt(livingYears[0]);
+        int endLiving = Integer.parseInt(livingYears[1]);
+        int timeLiving = endLiving - startLiving;
+
+        if (candidate.getAge() <= MIN_AGE && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && timeLiving >= MIN_TIME_LIVING_IN_UA) {
+            return true;
+        }
+        return false;
+    }
 }
